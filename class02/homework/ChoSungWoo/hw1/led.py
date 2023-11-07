8446a6addef62fe94a3d1b2f155841aeb1bb41e6
nnonames <24955405+nnonames@users.noreply.github.com>
2023-11-07 20:10:43 +0900

    Arduino_Python_Final

---
+++ class02/homework/ChoSungWoo/hw1/led.py
@@ -0,0 +1,31 @@
+#from iotdemo import FactoryController
+#__all__ = ('FactoryController')
+#ctrl = FactoryController('/dev/ttyACM0')
+
+from pyfirmata import Arduino
+import time
+
+arduino = Arduino('dev/ttyACM0')
+
+btn1 = arduino.get_pin()
+btn2 = arduino.get_pin()
+btn3 = arduino.get_pin()
+btn4 = arduino.get_pin()
+
+while True:
+	
+	if (btn1==0) :
+		
+	elif (btn1==1) :
+		
+	if (btn1==0) :
+	
+	elif (btn1==1) :
+		
+	if (btn1==0) :
+	
+	elif (btn1==1) :
+		
+	if (btn1==0) :
+
+	elif (btn1==1) :		
